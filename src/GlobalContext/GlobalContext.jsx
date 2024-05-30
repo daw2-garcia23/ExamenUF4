c433976373aa35afdfaa1d23deab05f7f95b291d
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2024-05-31 00:53:43 +0200

    d

--- src/GlobalContext/GlobalContext.jsx
+++ src/GlobalContext/GlobalContext.jsx
@@ -2,12 +2,8 @@ import { createContext, useState } from "react";
 
 export const GlobalContext = createContext()
 
-
-
 export function GlobalContextProvider({ children }) {
-
 const [contadorClicks, setContadorClicks] = useState(0);
-
 function clicks() {
     setContadorClicks(contadorClicks+1)
     console.log(contadorClicks)
