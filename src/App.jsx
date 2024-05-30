a891c1411fe2ea5b3f6645e45c16e4e0124fb91a
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2024-05-31 00:45:10 +0200

    preparación

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import { Home } from "./Vistas/Home"
 import { About } from "./Vistas/About"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Porsi } from "./Vistas/Porsi";
+import { GlobalContextProvider } from "./GlobalContext/GlobalContext";
+
 
 export default function App() {
   return (
-  <>
+  <GlobalContextProvider>
     <Header />
       <Routes>
         <Route path="/" element={<Home/>}> </Route>
         <Route path="/about" element={<About/>}> </Route>
         <Route path="/porsi" element={<Porsi />}></Route>
       </Routes>
-  </>
-  
+  </GlobalContextProvider>
   );
 }
